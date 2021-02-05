09cdbf0ee5c8ba5991ba0bd1b5cb48d47fce2ed3
YI Z <zhayi@umich.edu>
2021-02-04 23:06:11 -0500

    CI

--- bayes_CI_meta.R
+++ bayes_CI_meta.R
@@ -1,4 +1,4 @@
-bayespval_beta_meta<-function(beta,sd,r_vec = c(0, 1e-5, 6e-3, 0.024)){
+bayesCI_beta_meta<-function(beta,sd,r_vec = c(0, 1e-5, 6e-3, 0.024)){
   
   beta_o<-beta[1]
   beta_r<-beta[2]
@@ -18,7 +18,7 @@ bayespval_beta_meta<-function(beta,sd,r_vec = c(0, 1e-5, 6e-3, 0.024)){
   
   ####new grid
   eta2_vec = (beta_o^2+sigma_o)*c(1,2,4)
-
+  
   #pv = c(1.0, 0.99, 0.975, 0.95)
   rv = r_vec
   
@@ -35,30 +35,29 @@ bayespval_beta_meta<-function(beta,sd,r_vec = c(0, 1e-5, 6e-3, 0.024)){
   
   omg2 = grid[,1]
   phi2 = grid[,2]
-
+  
   mean<-sapply(1:length(omg2),function(x) 
     beta_o/(sigma_o/omg2[x]+phi2[x]/omg2[x]+1))
   
   var<-sapply(1:length(omg2),function(x) 
     (sigma_o+phi2[x])*omg2[x]/(sigma_o+phi2[x]+omg2[x])+phi2[x]+sigma_r)
   
-  
-  pval<-sapply(1:length(mean),function(x) pnorm(beta_r, mean=mean[x],sd=sqrt(var[x]))) 
-  
+   #pval<-sapply(1:length(mean),function(x) pnorm(beta_r, mean=mean[x],sd=sqrt(var[x]))) 
+  #integrate(normal_pdf(t,mean=mean[x],sd=sqrt(var[x])),lower=-Inf,upper=x )
   wts = dnorm(beta_o, mean=0, sd=sqrt(omg2+sigma_o+phi2))
   wts = wts/sum(wts)
-  
-  pval_wt<-wts%*%pval
-    
- #return(2*min(pval_wt,1-pval_wt))
-  return(pval_wt)
+
+  mixture_CDF_right<-Vectorize(function(t) wts%*%pnorm(t,mean=mean,sd=sqrt(var))-0.975)
+  root_right<-uniroot(mixture_CDF_right,c(mean(mean),10))$root  
+ 
+   mixture_CDF_left<-Vectorize(function(t) wts%*%pnorm(t,mean=mean,sd=sqrt(var))-0.025)
+  root_left<-uniroot(mixture_CDF_left,c(-10,mean(mean)))$root
+  return(c(root_left,root_right))
 }
-
-
 
 d<-read.table("rpp_extract.dat", head=T)
 attach(d)
-#rpp_data<-d[,c("beta_orig","se_orig","beta_rep","se_rep")]
-rpp_pval<-sapply(1:nrow(d),function(x) bayespval_beta_meta(beta=c(beta_orig[x], beta_rep[x]),sd=c(se_orig[x],  se_rep[x])))
+rpp_CI<-sapply(1:nrow(d),function(x) bayesCI_beta_meta(beta=c(beta_orig[x], beta_rep[x]),sd=c(se_orig[x],  se_rep[x])))
 
+rbind(rpp_CI,beta_rep)
 
